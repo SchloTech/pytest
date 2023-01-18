f3a647a580739f4f330df4f6a6040d734d3a27ef
Jake <jschlottag.1@gmail.com>
2023-01-18 06:10:45 -0500

    parrallel_testing

---
+++ parallel_testing/test_chem_results.py
@@ -0,0 +1,17 @@
+import time
+
+def test_result_1():
+    time.sleep(5)
+    print("Result #1 complete")
+
+def test_result_2():
+    time.sleep(5)
+    print("Result #2 complete")
+    
+def test_result_3():
+    time.sleep(5)
+    print("Result #3 complete")
+
+def test_result_4():
+    time.sleep(5)
+    print("Result #4 complete")
